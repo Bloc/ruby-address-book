28a9b5eb1cf4098143272eeb1745ead41fa91671
robmadden <rob.madden@bloc.io>
2015-05-05 14:55:23 -0700

    More updates to tests

--- specs/address_book_spec.rb
+++ specs/address_book_spec.rb
@@ -1,19 +1,23 @@
+require_relative "../models/address_book"
 
 RSpec.describe AddressBook do
-  book = AddressBook.new()
-  context "when adding an address" do
-    it "tests adding an entry to the AddressBook class" do
-      name = 'Winston Smith'
-      phone_number = '999.999.9999'
-      email = 'winston@winston.com'
-      book.add_entry(name, phone_number, email)
-      book_size = book.entries.size
-      expect $book_size == 1
+  let(:book) { AddressBook.new }
 
-      entry = book.entries.at(0)
-      expect entry.name.eql? name
-      expect entry.phone_number.eql? phone_number
-      expect entry.email.eql? email
+  describe ".add_entry" do
+    context "when adding an address" do
+      it "tests adding an entry to the AddressBook class" do
+        name = 'Winston Smith'
+        phone_number = '999.999.9999'
+        email = 'winston@winston.com'
+        book.add_entry(name, phone_number, email)
+        book_size = book.entries.size
+        expect $book_size == 1
+
+        entry = book.entries.at(0)
+        expect entry.name.eql? name
+        expect entry.phone_number.eql? phone_number
+        expect entry.email.eql? email
+      end
     end
   end
 
@@ -49,15 +53,16 @@ RSpec.describe AddressBook do
       expect entry.email.eql? "sussie@blocmail.com"
 
     end
-  end
 
-  context "when searching for an address" do
-    it "tests the binary_search method" do
-      entry = book.binary_search("Bob")
-      expect entry.name.eql? "Rob Madden"
-      expect entry.phone_number.eql? "630.768.6995"
-      expect entry.email.eql? "madden.rob@gmail.com"
+    context ".binary_search" do
+      #it "expect(AddressBook.binary_search.to return(1).item " do
+      it "should return Bob" do
+        book.add_from_csv("entries.csv")
+        entry = book.binary_search("Bob")
+        expect entry.name.eql? "Bob"
+        expect entry.phone_number.eql? "839-822-7831"
+        expect entry.email.eql? "bob@blocmail.com"
+      end
     end
   end
-
 end
--- specs/entry_spec.rb
+++ specs/entry_spec.rb
@@ -1,9 +1,10 @@
 require 'entry'
 
 RSpec.describe Entry, "#to_s" do
+  let(:entry) { Entry.new('Rob Madden', '630.768.6995', 'madden.rob@gmail.com') }
+
   context "when deserializing an address" do
     it "prints an entry as a string" do
-      entry = Entry.new('Rob Madden', '630.768.6995', 'madden.rob@gmail.com')
       expect entry.to_s.eql? "Name: #{entry.name}\nPhone Number: \##{entry.phone_number}\nEmail: #{entry.email}"
     end
   end
